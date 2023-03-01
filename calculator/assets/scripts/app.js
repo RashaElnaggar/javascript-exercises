0d55975d0e95270ce45196adc6777805db75995a
RashaElnaggar <rolly_elnaggar@yahoo.com>
2023-03-01 12:53:05 +0200

    first start

---
+++ calculator/assets/scripts/app.js
@@ -0,0 +1,53 @@
+const input1 =document.getElementById('input-number1');
+const input2 =document.getElementById('input-number2');
+const addbtn=document.getElementById('btn-add');
+const subbtn=document.getElementById('btn-subtract');
+const multplybtn=document.getElementById('btn-multiply');
+const devidebtn=document.getElementById('btn-divide');
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+  let res= 0;
+ currentResultOutput.textContent=res;
+
+  
+   
+   addbtn.addEventListener("click",add);
+   function add(){
+   var num1=parseInt(input1.value);
+     var num2 = parseInt(input2.value);
+     sumres = num1 + num2;
+    // var sumres=parseInt(input1.value)+parseInt(input2.value);
+    console.log(sumres);
+    currentResultOutput.textContent=sumres;
+     
+   }
+ subbtn.addEventListener("click",sub);
+
+function sub(){
+  var num1=parseInt(input1.value);
+  var num2 = parseInt(input2.value);
+   var subres=num1-num2;
+    console.log(subres);
+    currentResultOutput.textContent=subres;
+  
+  }
+ multplybtn.addEventListener('click',multiply);
+
+function multiply(){
+
+    var mulres=parseInt(input1.value)*parseInt(input2.value);
+    console.log(mulres);
+    currentResultOutput.textContent=mulres;
+  
+  }
+devidebtn.addEventListener('click',devide);
+
+function devide(){
+  
+    var devres=parseInt(input1.value)/parseInt(input2.value);
+    console.log(devres);
+    currentResultOutput.textContent=devres;
+  
+  }
